5d828eb3822b334996013bee3dec9a1bc6e7e490
Shashank Sharma <shashanksh2000@gmail.com>
2022-01-17 00:35:03 +0530

    Final commit

--- src/Create.js
+++ src/Create.js
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom";
 const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
-  const [author, setAuthor] = useState('mario');
+  const [author, setAuthor] = useState('');
   const history = useHistory();
 
   const handleSubmit = (e) => {
@@ -39,13 +39,12 @@ const Create = () => {
           onChange={(e) => setBody(e.target.value)}
         ></textarea>
         <label>Blog author:</label>
-        <select
+        <input 
+          type="text" 
+          required 
           value={author}
           onChange={(e) => setAuthor(e.target.value)}
-        >
-          <option value="mario">mario</option>
-          <option value="yoshi">yoshi</option>
-        </select>
+        />
         <button>Add Blog</button>
       </form>
     </div>
